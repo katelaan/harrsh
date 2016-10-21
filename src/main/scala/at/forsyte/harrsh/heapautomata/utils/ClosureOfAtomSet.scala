7bd50a7a4070200ed2e02f7730f82b5e34ca2735
jkatelaan <jens.katelaan@gmail.com>
2016-10-21 18:14:51 +0200

    ...

--- src/main/scala/at/forsyte/harrsh/heapautomata/utils/ClosureOfAtomSet.scala
+++ src/main/scala/at/forsyte/harrsh/heapautomata/utils/ClosureOfAtomSet.scala
@@ -10,49 +10,49 @@ import at.forsyte.harrsh.seplog.inductive.{PtrEq, PtrNEq, PureAtom}
 class ClosureOfAtomSet(pure : Set[PureAtom]) extends Closure {
 
   // FIXME: This closure class is ridiculously inefficient, having one copy of each equivalence class per member
-  var mapToClasses : Map[FV,Set[FV]] = Map()
-
-  for {
-    atom <- pure
-    (left, right, isEqual) = unwrapAtom(atom)
-    if isEqual
-  } {
-    extendEntry(left, right)
-  }
-
-  override def getEqualityClass(fv : FV) : Set[FV] = mapToClasses.getOrElse(fv, Set(fv))
-
-  override def isMinimumInItsClass(fv : FV) : Boolean = {
-    // If the EQ class is defined, check if i is the representation = the minimum of that class
-    // Otherwise, no equality for i has been set, so i is the unique and hence minimal element, so it is the representation
-    if (mapToClasses.isDefinedAt(fv)) {
-      mapToClasses(fv).min(Ordering.fromLessThan[PtrExpr]({
-        case p  => p._1 < p._2
-      })) == fv
-    } else {
-      true
-    }
-  }
-
-  private def extendEntry(key : FV, newVal : FV) = {
-    val eqClass = if (mapToClasses.isDefinedAt(key)) {
-      // Class is already defined, just add the new value
-      mapToClasses(key) + newVal
-    } else {
-      // Key not in any known eq class yet
-      // Either have to extend class for val, if known already, or create new class
-      if (mapToClasses.isDefinedAt(newVal)) mapToClasses(newVal) + key else Set(key, newVal)
-    }
-
-    // Extend entry for all members of the eq class
-    for {
-      classMember <- eqClass
-    } {
-      mapToClasses = mapToClasses + (classMember -> eqClass)
-    }
-  }
-
-  override lazy val asSetOfAtoms: Set[PureAtom] = EqualityUtils.propagateConstraints(pure)
+//  var mapToClasses : Map[FV,Set[FV]] = Map()
+//
+//  for {
+//    atom <- pure
+//    (left, right, isEqual) = unwrapAtom(atom)
+//    if isEqual
+//  } {
+//    extendEntry(left, right)
+//  }
+//
+//  override def getEqualityClass(fv : FV) : Set[FV] = mapToClasses.getOrElse(fv, Set(fv))
+//
+//  override def isMinimumInItsClass(fv : FV) : Boolean = {
+//    // If the EQ class is defined, check if i is the representation = the minimum of that class
+//    // Otherwise, no equality for i has been set, so i is the unique and hence minimal element, so it is the representation
+//    if (mapToClasses.isDefinedAt(fv)) {
+//      mapToClasses(fv).min(Ordering.fromLessThan[PtrExpr]({
+//        case p  => p._1 < p._2
+//      })) == fv
+//    } else {
+//      true
+//    }
+//  }
+//
+//  private def extendEntry(key : FV, newVal : FV) = {
+//    val eqClass = if (mapToClasses.isDefinedAt(key)) {
+//      // Class is already defined, just add the new value
+//      mapToClasses(key) + newVal
+//    } else {
+//      // Key not in any known eq class yet
+//      // Either have to extend class for val, if known already, or create new class
+//      if (mapToClasses.isDefinedAt(newVal)) mapToClasses(newVal) + key else Set(key, newVal)
+//    }
+//
+//    // Extend entry for all members of the eq class
+//    for {
+//      classMember <- eqClass
+//    } {
+//      mapToClasses = mapToClasses + (classMember -> eqClass)
+//    }
+//  }
+//
+//  override lazy val asSetOfAtoms: Set[PureAtom] = EqualityUtils.propagateConstraints(pure)
 
 //  override lazy val asSetOfAtoms: Set[PureAtom] = {
 //    val classes : Set[Set[FV]] = mapToClasses.values.toSet
@@ -74,7 +74,7 @@ class ClosureOfAtomSet(pure : Set[PureAtom]) extends Closure {
 //    eqClosure ++ neqClosure
 //  }
 
-  /*
+
 
   private val eqs : Set[PtrEq] = pure.filter(_.isInstanceOf[PtrEq]).map(_.asInstanceOf[PtrEq])
   private val neqs : Set[PtrNEq] = pure.filter(_.isInstanceOf[PtrNEq]).map(_.asInstanceOf[PtrNEq])
@@ -110,14 +110,14 @@ class ClosureOfAtomSet(pure : Set[PureAtom]) extends Closure {
   }
 
   override lazy val asSetOfAtoms: Set[PureAtom] = {
-    val eqs : Set[PureAtom] = (for {
+    val eqClosure : Set[PureAtom] = (for {
       c <- classes.values
       l <- c
       r <- c
       if l < r
     } yield PtrEq(l, r)).toSet
 
-    val neq : Set[PureAtom] = for {
+    val neqClosure : Set[PureAtom] = for {
       PtrNEq(l, r) <- neqs
       cl = getEqualityClass(l)
       cr = getEqualityClass(r)
@@ -125,9 +125,9 @@ class ClosureOfAtomSet(pure : Set[PureAtom]) extends Closure {
       rl <- cr
     } yield if (nl < rl) PtrNEq(nl, rl) else PtrNEq(rl, nl)
 
-    eqs ++ neqs
+    eqClosure ++ neqClosure
   }
 
-   */
+
 
 }
