c964be6c3d9bf7d53100b2f2b121dcebaf853851
Jens Katelaan <jkatelaan@forsyte.at>
2019-02-22 18:03:50 +0100

    Annotation with root parameters now works in additional cases.
    It no longer requires that rules allocate only at most one pointer.

--- src/main/scala/at/forsyte/harrsh/seplog/sidtransformers/AnnotateSidWithRootParams.scala
+++ src/main/scala/at/forsyte/harrsh/seplog/sidtransformers/AnnotateSidWithRootParams.scala
@@ -1,6 +1,6 @@
 package at.forsyte.harrsh.seplog.sidtransformers
 
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 import at.forsyte.harrsh.main.HarrshLogging
 import at.forsyte.harrsh.pure.ConstraintPropagation
 import at.forsyte.harrsh.seplog.{FreeVar, Var}
@@ -8,32 +8,22 @@ import at.forsyte.harrsh.seplog.inductive._
 
 object AnnotateSidWithRootParams extends HarrshLogging {
 
-  def apply(sid: Sid): Option[RichSid] = {
+  def apply(sid: Sid): Try[RichSid] = {
     Try {
-      val rootedPreds = sid.preds map (pred => (pred.head, rootOfPred(pred)))
+      val rootedPreds = sid.preds map (pred => (pred.head, uniqueRoot(pred)))
       RichSid(sid.startPred, sid.preds, sid.description, rootedPreds.toMap)
-    } match {
-      case Failure(exception) =>
-        logger.error(s"Can't annotate SID with root parameters: ${exception.getMessage}")
-        None
-      case Success(annotatedSID) => Some(annotatedSID)
     }
-  }
-
-  private def rootOfPred(pred: Predicate): FreeVar = {
-    // TODO: The following fails for SIDs that satisfy generalized progress but not strict progress (rootVars will then be empty). This could be fixed if we wanted to add support for generalized progress in the inputs (as opposed to only for the generated top-level predicates)
-    pred.rules.find(!_.satisfiesGeneralizedProgress(rootOfPred = None)).map {
-      rule => throw PreprocessingException(s"SID contains a rule that violates progress: $rule")
-    }
-    uniqueRoot(pred)
   }
 
   private def uniqueRoot(pred: Predicate): FreeVar = {
-    val allRoots = sourcesOfHeadPtrs(pred)
+    val allRoots = sourcesOfPtrs(pred)
+    logger.debug(s"Predicate $pred has roots: $allRoots")
+    // FIXME: If we know the dependencies / call graph for the predicates, we can avoid this by computing the root of the called predicates first
+    if (allRoots.isEmpty) throw PreprocessingException(s"Predicate $pred has no rule that allocates memory => Cannot add root")
     val intersection = allRoots.reduce(_ intersect _)
     val root = intersection.size match {
       case 0 =>
-        throw PreprocessingException(s"No unique root parameter in predicate $pred; roots by rule: ${allRoots.mkString(",")})")
+        throw PreprocessingException(s"Root parameter annotation: No unique root parameter in predicate $pred; roots by rule: ${allRoots.mkString(",")})")
       case 1 => intersection.head
       case _ =>
         logger.debug(s"Will arbitrarily pick root among free vars in $intersection")
@@ -42,15 +32,17 @@ object AnnotateSidWithRootParams extends HarrshLogging {
     root match {
       case v:FreeVar => v
       case v =>
-        throw PreprocessingException(s"Can't use non-free var $v as root.")
+        throw PreprocessingException(s"Root parameter annotation: Can't use non-free var $v as root.")
     }
   }
 
-  private def sourcesOfHeadPtrs(pred: Predicate): Seq[Set[Var]] = {
+  private def sourcesOfPtrs(pred: Predicate): Seq[Set[Var]] = {
     for {
       rule <- pred.rules
-      ptr <- rule.body.pointers.headOption
-    } yield ConstraintPropagation.closeSetUnderEqualities(Set(ptr.from), rule.body.pure)
+      if rule.body.hasPointer
+    } yield rule.body.pointers.toSet[PointsTo].flatMap {
+      ptr => ConstraintPropagation.closeSetUnderEqualities(Set(ptr.from), rule.body.pure)
+    }
   }
 
 }
