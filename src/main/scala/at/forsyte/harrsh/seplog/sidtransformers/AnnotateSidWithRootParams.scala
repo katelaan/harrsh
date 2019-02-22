6c2a05f161c9cff3646eb277dbf7f0492dcd7eb1
Jens Katelaan <jkatelaan@forsyte.at>
2019-02-22 19:02:58 +0100

    Annotation with root parameters is now a fixed-point computation.
    In SIDs that violate progress, we can now also discover the root
    parameters of predicates that have some rules that have calls but
    don't allocate memory, by propagating the root parameters of the
    called predicates (discovered in previous annotation iterations).
    
    This works for union types (which occur in the SL-COMP benchmarks) as
    well as other well-founded dependency hierarchies between predicates.

--- src/main/scala/at/forsyte/harrsh/seplog/sidtransformers/AnnotateSidWithRootParams.scala
+++ src/main/scala/at/forsyte/harrsh/seplog/sidtransformers/AnnotateSidWithRootParams.scala
@@ -6,42 +6,79 @@ import at.forsyte.harrsh.pure.ConstraintPropagation
 import at.forsyte.harrsh.seplog.{FreeVar, Var}
 import at.forsyte.harrsh.seplog.inductive._
 
+import scala.annotation.tailrec
+
 object AnnotateSidWithRootParams extends HarrshLogging {
 
   def apply(sid: Sid): Try[RichSid] = {
     Try {
-      val rootedPreds = sid.preds map (pred => (pred.head, uniqueRoot(pred)))
-      RichSid(sid.startPred, sid.preds, sid.description, rootedPreds.toMap)
+      val rootedPreds = annotationFixedPoint(sid, Map.empty)
+      RichSid(sid.startPred, sid.preds, sid.description, rootedPreds)
     }
   }
 
-  private def uniqueRoot(pred: Predicate): FreeVar = {
-    val allRoots = sourcesOfPtrs(pred)
+  @tailrec
+  private def annotationFixedPoint(sid: Sid, currentAnnotation: Map[String, FreeVar]): Map[String, FreeVar] = {
+    logger.debug("Starting annotation iteration from " + currentAnnotation)
+    if (sid.preds.size == currentAnnotation.size) {
+      currentAnnotation
+    }
+    else {
+      val discoveredRoots = for {
+        pred <- sid.preds
+        if !currentAnnotation.isDefinedAt(pred.head)
+        root <- uniqueRoot(currentAnnotation, pred, sid)
+      } yield (pred.head, root)
+      if (discoveredRoots.isEmpty) {
+        val unrooted = sid.preds.map(_.head).toSet[String] -- currentAnnotation.keySet
+        throw PreprocessingException(s"Cannot annotate predicates ${unrooted.mkString(",")} with roots in SID $sid")
+      } else {
+        val extendedAnnotation = currentAnnotation ++ discoveredRoots
+        annotationFixedPoint(sid, extendedAnnotation)
+      }
+    }
+  }
+
+  private def uniqueRoot(currentAnnotation: Map[String, FreeVar], pred: Predicate, sid: Sid): Option[FreeVar] = {
+    val allRoots = sourcesOfRules(currentAnnotation, pred, sid)
     logger.debug(s"Predicate $pred has roots: $allRoots")
-    // FIXME: If we know the dependencies / call graph for the predicates, we can avoid this by computing the root of the called predicates first
     if (allRoots.isEmpty) throw PreprocessingException(s"Predicate $pred has no rule that allocates memory => Cannot add root")
     val intersection = allRoots.reduce(_ intersect _)
-    val root = intersection.size match {
-      case 0 =>
-        throw PreprocessingException(s"Root parameter annotation: No unique root parameter in predicate $pred; roots by rule: ${allRoots.mkString(",")})")
-      case 1 => intersection.head
-      case _ =>
-        logger.debug(s"Will arbitrarily pick root among free vars in $intersection")
-        intersection.filter(_.isFree).head
-    }
-    root match {
-      case v:FreeVar => v
-      case v =>
-        throw PreprocessingException(s"Root parameter annotation: Can't use non-free var $v as root.")
+
+    if (intersection.isEmpty) {
+      None
+    } else {
+      if (intersection.size > 1) {
+        logger.debug(s"Will arbitrarily pick root among vars in $intersection")
+      }
+      intersection.headOption
     }
   }
 
-  private def sourcesOfPtrs(pred: Predicate): Seq[Set[Var]] = {
+  private def sourcesOfRules(currentAnnotation: Map[String, FreeVar], pred: Predicate, sid: Sid): Seq[Set[FreeVar]] = {
     for {
       rule <- pred.rules
-      if rule.body.hasPointer
-    } yield rule.body.pointers.toSet[PointsTo].flatMap {
-      ptr => ConstraintPropagation.closeSetUnderEqualities(Set(ptr.from), rule.body.pure)
+      if rule.body.hasPointer || rule.body.predCalls.nonEmpty
+    } yield sourcesOfSh(currentAnnotation, rule.body, sid)
+  }
+
+  private def sourcesOfSh(currentAnnotation: Map[String, FreeVar], body: SymbolicHeap, sid: Sid): Set[FreeVar] = {
+    if (body.hasPointer) {
+      body.pointers.toSet[PointsTo].flatMap {
+        ptr => Var.freeNonNullVars(ConstraintPropagation.closeSetUnderEqualities(Set(ptr.from), body.pure))
+      }
+    } else {
+      for {
+        call <- body.predCalls.toSet[PredCall]
+        root <- currentAnnotation.get(call.name).toSet[FreeVar]
+        index = sid(call.name).params.indexOf(root)
+        paramInstance = call.args(index)
+        eq_to_root <- ConstraintPropagation.closeSetUnderEqualities(Set(paramInstance), body.pure)
+        // Restrict to free vars here -- otherwise we might prematurely throw an exception,
+        // namely if there is a root variable, but the predicate for the call with the root variable
+        // is not yet in the currentAnnotation map
+        if eq_to_root.isFree
+      } yield eq_to_root.asInstanceOf[FreeVar]
     }
   }
 
